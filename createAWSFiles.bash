715a5c33bb53258ec6e7a23a1ecae9f0ca6020b2
Bill Froeschl <bfroeschl@gmail.com>
2024-09-10 17:44:33 +0000

    The Fix

--- createAWSFiles.bash
+++ createAWSFiles.bash
@@ -9,6 +9,7 @@ mkdir -p ~/.aws
 echo "[default]" >> ~/.aws/credentials
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
+echo "aws_session_token=$AWS_SESSION_TOKEN" >> ~/.aws/credentials
 
 # Create AWS config file ~/.aws
 echo "[default]" >> ~/.aws/config
