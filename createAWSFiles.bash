444bf0f1e14440e46b787f902a742f013121c415
Bill <bfroeschl@gmail.com>
2024-09-10 13:49:34 -0500

    The latest attempt

--- createAWSFiles.bash
+++ createAWSFiles.bash
@@ -9,8 +9,8 @@ mkdir -p ~/.aws
 echo "[default]" >> ~/.aws/credentials
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
-echo "aws_session_token=$AWS_SESSION_TOKEN" >> ~/.aws/credentials
+echo "region=$AWS_DEFAULT_REGION" >> ~/.aws/credentials
 
-# Create AWS config file ~/.aws
-echo "[default]" >> ~/.aws/config
-echo "region=$AWS_DEFAULT_REGION" >> ~/.aws/config
+# Create AWS config file ~/.aws 
+#echo "[default]" >> ~/.aws/config
+#echo "region=$AWS_DEFAULT_REGION" >> ~/.aws/config
