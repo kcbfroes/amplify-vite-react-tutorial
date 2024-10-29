393c913abfe2ba7fc5966bf06a25f545ad3ca0dd
Bill Froeschl <bfroeschl@gmail.com>
2024-10-29 22:46:09 +0000

    Looking good if I do say so myself. However, I'm having trouble re-showing Alert messages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import React, { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { Alert, Authenticator, CheckboxField, Flex, Heading, Text } from '@aws-amplify/ui-react'
+import { Alert, AlertVariations, Authenticator, Badge, CheckboxField, Flex, Heading, Text } from '@aws-amplify/ui-react'
 import Modal from "./components/Modal";
 import '@aws-amplify/ui-react/styles.css'
 import ListTodos from "./components/ListTodos";
 import TodoTS from "./components/TodoTS"
 import { emptyToDo, GraphQLFormattedError } from "./components/Interfaces";
+import { CONNECTION_STATE_CHANGE, ConnectionState } from 'aws-amplify/data';
+import { Hub } from 'aws-amplify/utils';
 
 const client = generateClient<Schema>();
 
 function App() {
+  
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
   const [createOpen, setCreateOpen] = useState(false)
   const [allData, setAllData] = useState(false)
-  const [errorMessage, setErrorMessage] = useState('')
-  const [errorHeading, setErrorHeading] = useState('')
   const [withError, setWithError] = useState(false)
+  const [alertHeading, setAlertHeading] = useState('')
+  const [alertMsg, setAlertMsg] = useState('')
+  const [alertVariation, setAlertVariation] = useState<AlertVariations>("info")
+  const [alertVisible, setAlertVisible] = useState(false)
+  const [subConnectVariation, setSubConnectVariation] = useState<AlertVariations>("info")
+  const [subConnectMsg, setsubConnectMsg] = useState<ConnectionState>()
 
   useEffect(() => {
     /*
@@ -28,7 +35,7 @@ function App() {
     const sub =client.models.Todo.observeQuery().subscribe({
       next: ({ items, isSynced }) => {
           setTodos([...items])
-          console.log("In App, useEffect refreshed Todos: ", todos)          
+          console.log("In App, useEffect refreshed Todos: ", items)          
           console.log("In App, useEffect isSynced: ", isSynced)
           setAllData(isSynced)
       },
@@ -40,6 +47,27 @@ function App() {
     emptyToDo.content = ''
     emptyToDo.isDone = false
   }
+
+  //------------------------------ Monitor Subscription Connection Status ------------------------------
+  Hub.listen('api', (data: any) => {
+    const { payload } = data;
+    if (payload.event === CONNECTION_STATE_CHANGE) {
+      const connectionState = payload.data.connectionState as ConnectionState;
+      //console.log(connectionState)
+      setsubConnectMsg(connectionState)
+      if (connectionState == "Connecting") {
+        setSubConnectVariation("info")
+      }else if (connectionState == "Connected") {
+        setSubConnectVariation("success")
+      }else{
+        setSubConnectVariation("error")
+        //also show an alert error message.
+        setAlertHeading("Backend Connection Lost")
+        setAlertMsg("The connection to the backend has been interupted. We'll keep trying to connect so no action is required on your parth")
+        setAlertVariation("error")
+      }
+    }
+  });
 
   //------------------------------ Create ------------------------------
   const newTodo = () => {
@@ -61,46 +89,80 @@ function App() {
 
       if (withError == false) {
         client.models.Todo.create({  content:aTodo.content, isDone:aTodo.isDone})
-          .then(result => handleResult(result, aTodo, "Todo Create"))
-          .catch(error => handleError(error, aTodo, "Todo Create"));
+          .then(result => handleTodoResult(result, aTodo, "Create a ToDo: " + aTodo.content))
+          .catch(error => handleTodoError(error, aTodo, "Create a ToDo: " + aTodo.content));
       }else{  
         //this call will generate a runtime error, which should be shown on the screen.   
         client.models.Todo.create(aTodo)
-          .then(result => handleResult(result, aTodo, "Todo Create"))
-          .catch(error => handleError(error, aTodo, "Todo Create"));
+          .then(result => handleTodoResult(result, aTodo, "Create a ToDo: " + aTodo.content))
+          .catch(error => handleTodoError(error, aTodo, "Create a ToDo: " + aTodo.content));
       }
     }
     setCreateOpen(false);
     setWithError(false)
+
   }
 
   //------------------------------ Edit/Update ------------------------------
   const updateTodo = (todo: Schema["Todo"]["type"]) => {
     console.log("In App, editTodo, update: ", todo)
     client.models.Todo.update(todo)
+      .then(result => handleTodoResult(result, todo, "Create a ToDo: " + todo.content))
+      .catch(error => handleTodoError(error, todo, "Create a ToDo: " + todo.content));
   }
 
   //------------------------------ Delete ------------------------------
   const deleteTodo = (id: string) => {
     console.log("In App, deleteTodo with id: ", id)
     client.models.Todo.delete({ id })
+      .then(result => handleTodoDeleteResult(result, id, "Deleting a ToDo"))
+      .catch(error => handleTodoDeleteError(error, id, "Deleting a ToDo"));
   }  
 
-  //------------------------------ Handle Todo Errors ------------------------------
-  const handleResult = (result: any, todo: Schema["Todo"]["type"], where: string ) => {
-    setErrorMessage('')
+  //------------------------------ Handle Todo Actions ------------------------------
+  const handleTodoResult = (result: any, todo: Schema["Todo"]["type"], what: string ) => {
+    setAlertMsg('')
     console.log("App, handleResult, result: ", result)
-    if (result.errors) handleError(result.errors, todo, where)    
-  }
-  const handleError = (errors: Array<GraphQLFormattedError>, todo: Schema["Todo"]["type"], where: string ) => {    
-    var allErrors: string = ''
-    setErrorHeading("Unexpeceted Error with Todo: '" + todo.content + "'")
-    for (const err of errors) {
-      allErrors += err.message + " (" + where + ")"
+    if (result.errors) {
+      handleTodoError(result.errors, todo, what)
+    }else{
+      setAlertHeading(what)
+      setAlertVariation("success")
+      setAlertVisible(true)
+      setAlertMsg(what + " was successful")
     }
-    console.log("App, handleError, allErrors: ", allErrors)
-    setErrorMessage(allErrors)
-    //console.log("handleError, errorMessage.length=", errorMessage.length)
+  }
+  const handleTodoError = (errors: Array<GraphQLFormattedError>, todo: Schema["Todo"]["type"], what: string ) => {    
+    var allErrors: string = ''
+    setAlertHeading("Unexpeceted Error")
+    for (const err of errors) {
+      allErrors += err.message + " (" + what + ")"
+    }
+    setAlertMsg(allErrors)
+    setAlertVariation("error")
+    setAlertVisible(true)
+  }
+  
+  const handleTodoDeleteResult = (result: any, todo_id: string, what: string ) => {
+    setAlertMsg('')
+    if (result.errors) {
+      handleTodoDeleteError(result.errors, todo_id, what)
+    }else{
+      setAlertHeading(what)
+      setAlertVariation("success")
+      setAlertVisible(true)
+      setAlertMsg(what + " was successful")
+    }
+  }
+  const handleTodoDeleteError = (errors: Array<GraphQLFormattedError>, todo_id: string, what: string ) => {    
+    var allErrors: string = ''
+    setAlertHeading("Unexpeceted Error " + what)
+    for (const err of errors) {
+      allErrors += err.message
+    }
+    setAlertMsg(allErrors)
+    setAlertVariation("error")
+    setAlertVisible(true)
   }
 
    //------------------------------ Optional UI Items ------------------------------
@@ -112,11 +174,17 @@ function App() {
     }    
   }
 
-  const showErrors = () => {
-    if (errorMessage.length > 0) {
+  const showAlerts = () => {
+    if (alertMsg.length > 0) {
       return(
-        <Alert variation="error" isDismissible={false} hasIcon={true} heading={errorHeading}>
-          {errorMessage}
+        <Alert 
+          variation={alertVariation} 
+          isDismissible={true} 
+          hasIcon={true} 
+          heading={alertHeading}
+          onDismiss={() => setAlertVisible(false)}
+        >
+          {alertMsg}
         </Alert>
       )
     }else{
@@ -144,6 +212,10 @@ function App() {
                 checked={withError ? true : false}
                 onChange={() => setWithError(true)} 
               />
+
+              <Badge size="large" variation={subConnectVariation}>
+                {subConnectMsg}
+              </Badge>
             </Flex>
 
             {newTodo()}
@@ -156,7 +228,7 @@ function App() {
 
           </main>
         <div>
-          {showErrors()}
+          {showAlerts()}
         </div>
         </React.Fragment>
       )}
